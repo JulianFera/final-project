38cb5386b6cc054bab090cdfe1112d8e582c25ff
Julian Fera <julianfera2@outlook.com>
2024-10-19 22:03:58 +0200

    main structure and added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Favorites from "./pages/Favorites";
+import SingelMovie from "./pages/SingelMovie";
 
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/singleMovie",
+      element: <SingelMovie />,
+    },
+    {
+      path: "/favorites",
+      element: <Favorites />,
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/SingelMovie.tsx
@@ -0,0 +1,3 @@
+export default function SingelMovie() {
+  return <div>SingeMovie</div>;
+}
