6434002168254a1e9e6210dd589281c69a6ae7f4
Julian Fera <julianfera2@outlook.com>
2024-10-21 19:16:25 +0200

    added Pagination component and API data

--- src/components/HomePage/movieList/MovieList.tsx
+++ src/components/HomePage/movieList/MovieList.tsx
@@ -1,15 +1,27 @@
+import { Pagination } from "antd";
 import useFetch from "../../../hooks/useFetch";
+import Error from "../../common/Error";
+import Loading from "../../common/Loading";
 import MovieCard from "../movieCart/MovieCart";
 import "./MovieList.css";
+import { useState } from "react";
 
 export default function MovieList() {
-  const { data: movies, loading, error } = useFetch();
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const { data: movies, loading, error } = useFetch(currentPage);
+
   if (loading) {
-    return <p>Loading...</p>;
+    return <Loading />;
   }
+
   if (error) {
-    return <p>Something went wrong...</p>;
+    return <Error />;
   }
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
     <div className="movie-card-container">
       {movies.map((movie) => {
@@ -22,6 +34,13 @@ export default function MovieList() {
           />
         );
       })}
+
+      <Pagination
+        current={currentPage}
+        onChange={handlePageChange}
+        showSizeChanger={false}
+        total={400}
+      />
     </div>
   );
 }
